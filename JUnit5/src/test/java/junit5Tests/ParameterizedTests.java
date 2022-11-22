a285ff25a83111a22acf3de789bfddf05943c6da
Movila-Stefan <movila.vl@gmail.com>
2022-11-22 22:29:06 +0200

    3

--- JUnit5/src/test/java/junit5Tests/ParameterizedTests.java
+++ JUnit5/src/test/java/junit5Tests/ParameterizedTests.java
@@ -1,10 +1,7 @@
 package junit5Tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class ParameterizedTests {
 
@@ -15,12 +12,38 @@ public class ParameterizedTests {
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
+    @NullAndEmptySource // face ce fac cele de mai jos
  //   @NullSource
  //   @EmptySource
     @ValueSource(strings = {"firstString", "secondString"})
     void stringValues(String theParam){
+
         System.out.println("theParam = " + theParam);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,rogers", "capitan,marvel", "bucky,barnes"})
+    void csvSource_StringString(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,32,true", "capitan,21,false", "bucky,5,true"})
+    void csvSurce_StringIntBoolean(String param1, int param2, boolean param3){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america, 'steve,rogers'", "winter solder,"+"'bucky, barnes'"})
+    void csvSource_StringWithComa(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+        void csvSource_StringWithDiffDelimiter(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
 
 }
