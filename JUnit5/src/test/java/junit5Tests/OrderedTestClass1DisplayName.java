97e25ed4ad90782dd2e0240b6bdf26b481c1b62c
Movila-Stefan <movila.vl@gmail.com>
2022-12-07 23:02:50 +0200

    @TestMethodOrder

---
+++ JUnit5/src/test/java/junit5Tests/OrderedTestClass1DisplayName.java
@@ -0,0 +1,47 @@
+package junit5Tests;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderedTestClass1DisplayName {
+
+    @BeforeAll
+    void beforeAll(){
+        System.out.println("--this is the before All method");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("----This is the before Each method");
+    }
+
+    @AfterAll
+    void afeterAll(){
+        System.out.println("-- this is the after All method");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("----this is the after Each method");
+    }
+
+    @Test
+    void firstMethod() {
+        System.out.println("first test method");
+    }
+
+    @Test
+    @DisplayName("US1234 - TC12 - this method is the second one ")
+    void secondMethod() {
+        {
+            System.out.println("second method");
+        }
+
+    }
+    @Test
+    @DisplayName("A display name")
+    void thirdTest() {
+        System.out.println("This is the third test method");
+    }
+}
